cbd704a9a810ed019a36ac67b9fc349038a5714b
houk1987 <houk1987@houk1987s-Air.lan>
2026-02-14 08:44:35 +0800

    config buffer and keymaps

--- init.lua
+++ init.lua
@@ -2,9 +2,10 @@ local option = vim.opt
 local buffer = vim.b
 local global = vim.g
 
+-- Option Setting --
+
 option.showmode = true
 option.expandtab = true
-option.showmode = false
 option.tabstop = 4
 option.shiftwidth = 4
 option.shiftround = true
@@ -29,3 +30,41 @@ option.mouse = "a"
 option.undofile = true
 option.undodir = vim.fn.expand('$HOME/.local/share/nvim/undo')
 option.exrc=true
+option.wrap=false
+option.splitright=true
+option.splitbelow=true
+
+-- Buffer Settings --
+
+buffer.fileencoding="utf-8"
+
+
+-- Global Settings --
+
+global.mapleader = " "
+
+local keymap = vim.keymap.set
+
+-- Key Mappings -- 
+
+-- cancle Left Right Up Down -- 
+keymap({"n","i"},'<Left>','<Nop>')
+keymap({"n","i"},'<Right>','<Nop>')
+keymap({"n","i"},'<Up>','<Nop>')
+keymap({"n","i"},'<Down>','<Nop>')
+keymap({"i"},'jk','<esc>')
+
+-- Alt Tab -- 
+keymap("n","<A-Tab>","<cmd>bNext<CR>",{silent = true})
+
+-- close buffer -- 
+keymap("n","<leader>bc","<cmd>bd<CR>",{silent = true})
+
+-- write Buffer -- 
+keymap("n","<leader>w","<cmd>w<CR>",{silent = true})
+
+-- move line up or down --
+keymap("v","J",":m '>+1<CR>gv=gv",{silent = true})
+keymap("v","K",":m '<-2<CR>gv=gv",{silent = true})
+
+
