8fb14c70d73198225aea80215bef106b72ccf081
houk1987 <houk1987@houk1987s-Air.lan>
2026-02-15 07:26:14 +0800

    add theme and change ui

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,24 @@
+return{
+    "folke/tokyonight.nvim",
+    dependencies={
+        'nvim-lualine/lualine.nvim',
+        "nvim-tree/nvim-tree.lua",
+        "utilyre/barbecue.nvim",
+        'nvim-tree/nvim-web-devicons',
+        "SmiteshP/nvim-navic",
+    },
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.cmd[[colorscheme tokyonight-storm]]
+        require('lualine').setup({
+            options = {
+                theme ='tokyonight'
+            }
+        })
+        require("nvim-tree").setup()
+        require("barbecue").setup{
+            theme ='tokyonight'
+        }
+    end
+}
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,26 +1,16 @@
 return{
-    "folke/tokyonight.nvim",
-    dependencies={
-        'nvim-lualine/lualine.nvim',
-        "nvim-tree/nvim-tree.lua",
-        "utilyre/barbecue.nvim",
-        'windwp/nvim-autopairs',
-        'nvim-tree/nvim-web-devicons',
-        "SmiteshP/nvim-navic",
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        config = true
     },
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd[[colorscheme tokyonight-storm]]
-        require('lualine').setup({
-            options = {
-                theme ='tokyonight'
-            }
-        })
-        require("nvim-tree").setup()
-        require("barbecue").setup{
-            theme ='tokyonight'
-        }
-        require("nvim-autopairs").setup{}
-    end
+    {
+        'akinsho/bufferline.nvim',
+        config = true
+    },
+    {
+        'windwp/nvim-autopairs',
+        config = true
+    }
 }
+
